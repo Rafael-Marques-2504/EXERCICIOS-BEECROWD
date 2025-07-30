0686dfc7719b9520e26556c86e57c27b3ac87086
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-07-30 17:10:41 -0300

    1065 resolvido

---
+++ 1065-NVL1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int contagem = 0;
+        int pares = 0;
+        int numero = 0;
+        while (contagem < 5)
+        {
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                pares++;
+            }
+            contagem++;
+
+        }
+        Console.WriteLine(pares + " valores pares");
+    }
+   
+}
+
