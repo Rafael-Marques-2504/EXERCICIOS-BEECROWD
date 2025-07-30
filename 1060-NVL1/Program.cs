68ed52dc582e6901d70bf5fb07e0f6c54aa586c2
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-07-30 16:41:39 -0300

    1060 resolvido

---
+++ 1060-NVL1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double n = 0;
+        double pos =0;
+        for (int i = 0; i < 6; i++)
+        {
+           n = double.Parse(Console.ReadLine());
+            if (n != 0)
+            {
+                if (n > 0)
+                {
+                    pos = pos + 1;
+
+                }
+            }
+        }
+        Console.WriteLine(pos + " valores positivos");
+    }
+
+}
