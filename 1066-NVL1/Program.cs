05ab0d16a28d6c02aceb7e8f9298ea74d4c656d6
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-07-31 09:00:04 -0300

    1066 NVL 1 resolvido

---
+++ 1066-NVL1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int n = 0;
+        int neg = 0;
+        int pos = 0;
+        int par = 0;
+        int imp = 0;
+        for (int i = 0; i < 5; i++)
+        {
+
+            n = int.Parse(Console.ReadLine());
+            if (n > 0) { pos++; }
+            if (n < 0) { neg++; }
+            if (n % 2 == 0) { par++; }
+            if (n % 2 != 0) { imp++; }
+
+        }
+        Console.WriteLine(par + " valor(es) par(es)");
+        Console.WriteLine(imp + " valor(es) impar(es)");
+        Console.WriteLine(pos + " valor(es) positivo(s)");
+        Console.WriteLine(neg + " valor(es) negativo(s)");
+    }
+}
