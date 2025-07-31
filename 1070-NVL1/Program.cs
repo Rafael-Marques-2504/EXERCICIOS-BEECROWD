3e8855ffaca1e1f984d149f4ffe009ee8f3b719f
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-07-31 09:29:00 -0300

    1070-NVL1 resolvido

---
+++ 1070-NVL1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+        int x = int.Parse(Console.ReadLine());
+        int contagem = 0;
+        while (contagem < 6)
+        {
+            if (x % 2 == 0)
+            {
+             x++;
+             Console.WriteLine(x);
+             x = x + 2;
+            }
+            else
+            {
+        Console.WriteLine(x);
+        x = x + 2;
+            }
+            contagem++;
+                        
+        }
+    
