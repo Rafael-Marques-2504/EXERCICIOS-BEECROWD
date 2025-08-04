c8d988ac08ddb73d9e6846ee29da1d3d7852d62f
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-04 09:23:38 -0300

    1078 resolvido

---
+++ 1078-NVL1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int resultado = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            resultado = i * n;
+            Console.WriteLine(i + " x " + n + " = " + resultado);
+
+
+        }
+
+    }
+}
