983b27b78632f6e0a7b5bbdb5028c6f03028d11d
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-04 10:05:52 -0300

    1079 resolvido

---
+++ 1079-NVL1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] entrada;
+        double n1 = 0;
+        double n2 = 0;
+        double n3 = 0;
+        double mediaPonderada = 0;
+        for (int i = 0; i < n; i++)
+        {
+            entrada = Console.ReadLine().Split(' ');
+            n1 = double.Parse(entrada[0]);
+            n2 = double.Parse(entrada[1]);
+            n3 = double.Parse(entrada[2]);
+            mediaPonderada = (n1 * 2 + n2 * 3 + n3 * 5) / 10;
+            Console.WriteLine(mediaPonderada.ToString("F1"));
+
+        }
+
+
+
+    }
+
+}
