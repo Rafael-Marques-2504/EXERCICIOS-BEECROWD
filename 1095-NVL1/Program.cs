37ff6d92c447e59c435cdc972f2889eb4febb163
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-05 17:38:51 -0300

    1095 resolvido

---
+++ 1095-NVL1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int j = 60; 
+        while(j >= 0)
+        {
+            Console.WriteLine("I=" + i + " J=" + j);
+            i = i + 3;
+            j = j - 5;
+        }
+    }
+
+}
