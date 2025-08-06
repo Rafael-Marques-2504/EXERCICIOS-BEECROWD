f090752c626be87762a496d214c9d98f6a393088
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-06 16:54:03 -0300

    1096 resolvido

---
+++ 1096-NVL1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int j = 7;
+        for (i = 1; i <= 9; i = i + 2)
+        {
+            for (j = 7; j >= 5; j--)
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+            }
+        }
+    }
+
+}
