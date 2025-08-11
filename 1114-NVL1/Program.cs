eeabfb63b1cc1d30b0b895776b3c8df167157350
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-11 10:49:10 -0300

    1114 resolvido

---
+++ 1114-NVL1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int x = 0;
+        while (x != 2002)
+        {
+            x = int.Parse(Console.ReadLine());
+            if( x != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+            }
+            else
+            {
+                Console.WriteLine("Acesso Permitido");
+
+            }
+        }
+
+    }
+
+}
