25365cc040d4f010cd186f5192479fece70f5a16
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-11 10:42:28 -0300

    1113 resolvido

---
+++ 1113-NVL1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int x, y;
+
+        while (true)
+        {
+            string[] partes = Console.ReadLine().Split(' ');
+            x = int.Parse(partes[0]);
+            y = int.Parse(partes[1]);
+
+            if (x == y)
+            {
+                break; // encerra o loop
+            }
+            else if (x > y)
+            {
+                Console.WriteLine("Decrescente");
+            }
+            else
+            {
+                Console.WriteLine("Crescente");
+            }
+        }
+    }
+}
