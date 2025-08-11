e848ad31a40891697638d850355dbdc33527000a
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-11 10:29:06 -0300

    1099 resolvido

---
+++ 1099-NVL1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int X = int.Parse(entrada[0]);
+            int Y = int.Parse(entrada[1]);
+
+            int menor = Math.Min(X, Y);
+            int maior = Math.Max(X, Y);
+
+            int soma = 0;
+
+            for (int j = menor + 1; j < maior; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    soma += j;
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
