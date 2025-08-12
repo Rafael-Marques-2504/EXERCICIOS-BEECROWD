d8551e790b2da51cb189aa48cc075365b037fbdb
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-12 15:38:01 -0300

    1009 resolvido - Assunto: Sequencial

---
+++ 1009-NVL2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string nome = Console.ReadLine();
+        double SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double TotalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double Total = SalarioFixo + TotalVendas * 0.15;
+        Console.WriteLine("TOTAL = R$ " + Total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+     
+
+    }
+
+}
