9ceb8a3a84011c2e383cea80a8ffb6bc674cf77c
Rafael Marques Silva <160083967+Rafael-Marques-2504@users.noreply.github.com>
2025-08-12 15:26:24 -0300

    1008-NVL2/ Assunto: Sequencial

---
+++ 1008-NVL2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+
+    {
+        int NumeroFuncionario = int.Parse(Console.ReadLine());
+        double HorasTrabalhadas = double.Parse(Console.ReadLine());
+        double ValorHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        double salario = HorasTrabalhadas * ValorHora;
+        Console.WriteLine("NUMBER = " + NumeroFuncionario);
+        Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
